8ba4420151aa36972f3a41cd01e17e5e4ce1239f
kwhjvdkamp <info@van-der-kamp.nl>
2020-06-25 23:23:59 +0200

    wip

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -21,25 +21,51 @@ const HABITS_QUERY = gql`
   }
 `;
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// App.js
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const { data, loading, error } = useQuery(HABITS_QUERY);
+  
+    if (loading) {
+      return <p>loading...</p>;
+    }
+  
+    if (error) {
+      return <p>Ruh roh! {error.message}</p>;
+    }
+  
+    return (
+      <>
+        <h2>Habits</h2>
+        <ul>
+          {data.habits.map((habit) => {
+            return (
+              <li key={habit.id}>{habit.description}</li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
 
 export default App;
